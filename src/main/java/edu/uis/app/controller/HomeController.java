f46fd811f795d0f1501d787a66d03c2f360e7837
Pablo Delgado <pablo.delgado@gmx.com>
2017-04-26 14:20:17 -0400

    Added analytic service for home dashboard

--- src/main/java/edu/uis/app/controller/HomeController.java
+++ src/main/java/edu/uis/app/controller/HomeController.java
@@ -1,24 +1,33 @@
 package edu.uis.app.controller;
 
+import edu.uis.app.service.AnalyticService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- *
- * @author Pablo Delgado <pdelg2@uis.edu>
+ * @author Pablo Delgado
  */
 @Controller
 @RequestMapping("/")
 public class HomeController {
     
-    @RequestMapping(method = RequestMethod.GET)
-    public String home(Model model) {    
-        model.addAttribute("welcomeMessage", "Welcome to UIS Alumni Tracker");
-        return "home";
+    private AnalyticService analyticService;
+
+    @Autowired
+    public void setAnalyticService(AnalyticService service) {
+        analyticService = service;
     }
     
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(Model model) {
+        model.addAttribute("studentAlumniCount", analyticService.studentAlumniCount());
+        model.addAttribute("graduatedAlumniCount", analyticService.graduatedAlumniCount());
+        model.addAttribute("employerCount", analyticService.employerCount());
+        return "home";
+    }
     
     /**
      * Used for developing and testing layouts, it should be deleted when no
---
+++ src/main/java/edu/uis/app/service/AnalyticService.java
@@ -0,0 +1,49 @@
+package edu.uis.app.service;
+
+import edu.uis.app.jpa.Alumni;
+import edu.uis.app.jpa.AlumniRepository;
+import edu.uis.app.jpa.Employer;
+import edu.uis.app.jpa.EmployerRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Pablo Delgado
+ */
+@Service
+public class AnalyticService {
+    
+    private AlumniRepository alumniRepository;
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    public void setAlumniRepository(AlumniRepository alumniRepository) {
+        this.alumniRepository = alumniRepository;
+    }
+
+    @Autowired
+    public void setEmployerRepository(EmployerRepository employerRepository) {
+        this.employerRepository = employerRepository;
+    }
+
+    public Integer studentAlumniCount() {
+        List<Alumni> students = alumniRepository.findByGraduated(Boolean.FALSE);
+        if(students == null) return 0;
+        else return students.size();
+    }
+
+    public Integer graduatedAlumniCount() {
+        List<Alumni> students = alumniRepository.findByGraduated(Boolean.TRUE);
+        if(students == null) return 0;
+        else return students.size();
+    }
+
+    public Integer employerCount() {
+        List<Employer> employers = employerRepository.findAll();
+        if(employers == null) return 0;
+        else return employers.size();
+    }
+    
+}
